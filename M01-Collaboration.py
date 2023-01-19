b949bdc01ac5fd573a4b73969f631ecf475512b0
Allene <anewport4@ivytech.edu>
2023-01-19 15:55:47 -0500

    M01 Assignment

---
+++ M01-Collaboration.py
@@ -0,0 +1,10 @@
+#############################################
+#
+# M01 Assignment - Collaboration
+# Author - Allene Cates
+# Created - 1/17/2023
+# Simple print statement that says Hello World!
+#
+#############################################
+
+print("Hello World!")
